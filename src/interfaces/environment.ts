7c50247deaff925c4299a5e0f43c4ad75c30b5a9
Bartłomiej Szal <bartlomiej.szal@it.integro.pl>
2019-05-24 12:50:22 +0200

    Update environment structure and schema

--- src/interfaces/environment.ts
+++ src/interfaces/environment.ts
@@ -1,4 +1,9 @@
 export interface Environment {
+    type: EnvironmentType;
+    container: {
+        name: string;
+        serverInstance: string;
+    }
     database: {
         databaseName: string;
         // Feature not implemented yet
@@ -12,4 +17,9 @@ export interface Environment {
         navModelToolsPath: string,
         navAdminToolPath: string
     };
+}
+
+export enum EnvironmentType {
+    local = "Local",
+    container = "Container"
 }
